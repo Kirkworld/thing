b229841b82f476007a2c13ce8cb2deeab680d108
Kirkwold <goldgomaster@gmail.com>
2018-02-03 22:27:49 +0000

    New bit of story & parental lock
    YAAYYYY

--- thething.py
+++ thething.py
@@ -3,5 +3,16 @@ input()
 print("By Archie Alexander")
 input()
 print("Version 0.002")
+lock = input("Do want lock")
 derick = input("WHAT IS YOUR NAME?")
-print("well", derick ,"go away")
+print("well", derick)
+input()
+print("...")
+input()
+print("I'm going to execute you little")
+print("Annoying")
+if lock == "yes":
+    print ("you little shithead")
+else:
+    print("you little Dimwit")
+#I don't know have you got anything
